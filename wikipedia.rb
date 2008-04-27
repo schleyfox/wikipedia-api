5a008437da8a89d682a68b4626f0bdc50e4fb760
Ben Hughes <ben@pixelmachine.org>
2008-04-27 15:32:14 -0400

    Find by titles and by pageids
    * prop and revids

--- wikipedia.rb
+++ wikipedia.rb
@@ -2,6 +2,7 @@
 
 class Wikipedia
   BASE_URL = 'http://en.wikipedia.org/w/api.php?format=xml&'
+  OPTS = [:revids, :prop]
   PROPS = [:info, :revisions, :links, :langlinks, :images, :imageinfo,
     :templates, :categories, :extlinks, :categoryinfo]
 
@@ -12,12 +13,16 @@ class Wikipedia
     @pages = (@xml/:api/:query/:pages/:page).collect{|p| Page.new(p) }
   end
 
-  def self.find(page_ids, props = nil)
-    props ||= PROPS
-    props = props & PROPS
-    page_id_qs = "pageids=#{CGI.escape(page_ids.join('|'))}"
-    props_qs = "prop=#{CGI.escape(props.join('|'))}"
-    Wikipedia.new(make_url(page_id_qs, props_qs))
+  def self.find_by_pageids(page_ids, opts = nil)
+    opts_qs = handle_options(opts)
+    page_ids_qs = "pageids=#{CGI.escape(page_ids.join('|'))}"
+    Wikipedia.new(make_url(opts_qs.push(page_ids_qs)))
+  end
+
+  def self.find_by_titles(titles, opts = nil)
+    opts_qs = handle_options(opts)
+    titles_qs = "titles=#{CGI.escape(titles.join('|'))}"
+    Wikipedia.new(make_url(opts_qs.push(titles_qs)))
   end
 
   class Page
@@ -36,5 +41,14 @@ class Wikipedia
   protected
   def self.make_url(*opts)
     BASE_URL + (["action=query"] + opts).join('&')
+  end
+
+  def self.handle_options(opts)
+    opts ||= {}
+    opts[:prop] ||= PROPS
+    opts[:prop] = opts[:prop] & PROPS
+    res = ["prop=#{CGI.escape(opts[:prop].join('|'))}"]
+    res << "revids=#{CGI.escape(opts[:revids].join('|'))}" if opts[:revids]
+    res
   end
 end
